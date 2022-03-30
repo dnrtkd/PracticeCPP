319a30a51b85bd354041576448cdaedf0502f079
gma9280 <dnrtkd545@naver.com>
2022-03-30 20:52:07 +0900

    step3

---
+++ cStep1/cStep1/c.cpp
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	정수
+	char = 1byte
+	short=2byte
+	int=4byte
+	long=4byte
+
+	실수
+	float=4byte
+	double=8byte
+
+	*/
+	
+	/*
+	+
+	-
+	*
+	%
+	/
+
+	=
+	// 한 줄 주석
+	 
+
+
+*/
+
+	//몬스터
+	int i = 0;
+
+	int j = 0; //몬스터
+
+	/*
+	. 닷
+	, 콤마
+	: 콜론
+	; 세미 콜론
+	"" 문자열
+	'' 문자
+	* 에스터리스크
+	& 엠퍼센트
+	() 소괄호
+	{} 중괄호
+	[] 대괄호
+
+	& and연산
+	| or연산
+	! not 연산
+	^ xor 연산
+	*/
+			
+			
+    		
+			
+	return 0;
+}
